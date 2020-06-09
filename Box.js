60f97451ce851195b90e6f63926b612918f24dd8
abhishek1501kholiya <abhishekkholiya15012007@gmail.com>
2020-06-09 09:25:54 +0530

    added vanishing effects of blocks

--- Box.js
+++ Box.js
@@ -10,17 +10,34 @@ class Box{
       this.width = width;
       this.height = height;
       this.image = loadImage("sprites/wood1.png");
+      this.visibility = 255;
       World.add(world, this.body);
     }
     display(){
+
+      
       var angle = this.body.angle;
+      if(this.body.speed<3.5){
       push();
+      console.log(this.body.speed);
       translate(this.body.position.x, this.body.position.y);
       rotate(angle);
       imageMode(CENTER);
       image(this.image, 0, 0, this.width, this.height);
       pop();
+      }
+      else{
+        World.remove(world,this.body);
+        push();
+        this.visibility = this.visibility -5;
+       tint(255,this.visibility);
+       image(this.image,this.body.position.x,this.body.position.y,20,20);
+       pop()
+      }
     }
+    
+
+    
 }
 //class Box extends BaseClass {
   //constructor(x, y, width, height){
